8fb1cba64ca477d21942fad5403abf6775375bdb
Niladri Chatterjee <Niladri@niladris-mbp>
2018-03-18 00:23:34 +0000

    Initial Commit

---
+++ IRAPIPoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IRAPIPoc
+//
+//  Created by Niladri Chatterjee on 18/03/2018.
+//  Copyright © 2018 Niladri Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
